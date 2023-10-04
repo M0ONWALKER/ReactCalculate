1cbaa2f36fbdfc25c4f0a1e3e03bc253ec8e2be3
M0ONWALKER <rick.sanchez,75@mail.ru>
2023-10-04 16:15:41 +0300

    Foundation preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+  }
+
+
+  render(){
+    return (
+      <main className="main">
+        <div className="main__calculator">
+          <input className="calculator__input" type="text" placeholder="0"></input>
+          <div className="calculator__buttons">
+            <button className="calculator__btn">2</button>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
